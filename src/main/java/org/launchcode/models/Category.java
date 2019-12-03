91c228bbb8e0eb59c2e11a100432fbcd0c687c9a
Wesley Park <wespark25@gmail.com>
2019-12-03 11:07:13 -0600

    Part 1 of Studio: Cheese MVC Persistent completed
    The frontend could be touched up a bit to make things a little prettier. The backend is completely fully functional and where it should be.
    
    On to Part 2!

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,36 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void Category() { }
+    public void Category(String name) {
+        this.name = name;
+    }
+}
